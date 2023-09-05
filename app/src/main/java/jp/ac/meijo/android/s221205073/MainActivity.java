2ba7fd56fb61001326c65b3f8db2897355ef4f6f
So Sakamaki <115204648+Sakamaki-So@users.noreply.github.com>
2023-09-05 13:39:22 +0900

    change activity_main

--- app/src/main/java/jp/ac/meijo/android/s221205073/MainActivity.java
+++ app/src/main/java/jp/ac/meijo/android/s221205073/MainActivity.java
@@ -2,13 +2,38 @@ package jp.ac.meijo.android.s221205073;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import jp.ac.meijo.android.s221205073.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.textButton.setOnClickListener(v -> {
+            var text = binding.editText.getText().toString();
+            binding.editText.setText(text);
+        });
+
+        binding.editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.activityMainText.setText(editable.toString());
+            }
+        });
     }
 }
